9b33abe44fe27d83cbd5c11de0ea18acb4e4bd05
saralune <sarahlef59@hotmail.fr>
2024-11-08 12:05:08 +0100

    Initial Commit

---
+++ MemeGenerator/MemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeGeneratorApp.swift
+//  MemeGenerator
+//
+//  Created by Sara Lefort on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
